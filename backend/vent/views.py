2bd4411074269a661897dc23b2fa8b8142458099
Jake Richards <contact@jakerichards.info>
2024-11-09 20:43:36 -0800

    dockerfile setup and boilerplating

--- backend/vent/views.py
+++ backend/vent/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from vent import models
 
-# Create your views here.
+
+class Test(APIView):
+    def get(self, request):
+        return Response("Hello, world!")
