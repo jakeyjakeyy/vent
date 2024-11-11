3b70254c2b445c51659c8b893d580b2a7036fc6f
Jake Richards <contact@jakerichards.info>
2024-11-10 17:40:31 -0800

    basic master collision checking in cloud

--- frontend/src/components/Cloud.vue
+++ frontend/src/components/Cloud.vue
@@ -2,20 +2,106 @@
 import { ref } from "vue";
 import Droplet from "./Droplet.vue";
 
+type Droplet = {
+  content: string;
+  id: number;
+  x: number;
+  y: number;
+  vx: number;
+  vy: number;
+  mass: number;
+  dropletWidth: number;
+  dropletHeight: number;
+};
+
 let posts = ref([
-  { content: "Hello, World!", id: 1 },
-  { content: "Hello, Vue!", id: 2 },
-  { content: "Hello, vent!", id: 3 },
+  {
+    content: "Hello, World!",
+    id: 1,
+    x: 0,
+    y: 0,
+    vx: 0,
+    vy: 0,
+    mass: 0,
+    dropletWidth: 0,
+    dropletHeight: 0,
+  },
+  {
+    content: "Hello, Vue!",
+    id: 2,
+    x: 0,
+    y: 0,
+    vx: 0,
+    vy: 0,
+    mass: 0,
+    dropletWidth: 0,
+    dropletHeight: 0,
+  },
+  {
+    content: "Hello, vent!",
+    id: 3,
+    x: 0,
+    y: 0,
+    vx: 0,
+    vy: 0,
+    mass: 0,
+    dropletWidth: 0,
+    dropletHeight: 0,
+  },
   {
     content: "A very long post that is at the limits of our maximum characters",
     id: 4,
+    x: 0,
+    y: 0,
+    vx: 0,
+    vy: 0,
+    mass: 0,
+    dropletWidth: 0,
+    dropletHeight: 0,
   },
 ]);
+
+function updatePost(updatedDroplet: Droplet) {
+  const index = posts.value.findIndex((post) => post.id === updatedDroplet.id);
+  posts.value[index] = updatedDroplet;
+
+  // Check for collisions
+  for (const post of posts.value) {
+    if (post.id === updatedDroplet.id) continue;
+    if (
+      updatedDroplet.x < post.x + post.dropletWidth &&
+      updatedDroplet.x + updatedDroplet.dropletWidth > post.x &&
+      updatedDroplet.y < post.y + post.dropletHeight &&
+      updatedDroplet.y + updatedDroplet.dropletHeight > post.y
+    ) {
+      if (
+        updatedDroplet.x === post.x + post.dropletWidth ||
+        updatedDroplet.x + updatedDroplet.dropletWidth === post.x
+      ) {
+        updatedDroplet.vx *= -1;
+        post.vx *= -1;
+      }
+      if (
+        updatedDroplet.y === post.y + post.dropletHeight ||
+        updatedDroplet.y + updatedDroplet.dropletHeight === post.y
+      ) {
+        updatedDroplet.vy *= -1;
+        post.vy *= -1;
+      }
+    }
+  }
+}
 </script>
 
 <template>
   <div class="cloud-container">
-    <Droplet v-for="post in posts" :key="post.id" :post="post" />
+    <Droplet
+      v-for="post in posts"
+      :key="post.id"
+      :post="post"
+      :posts="posts"
+      @update="updatePost"
+    />
   </div>
 </template>
 
--- frontend/src/components/Droplet.vue
+++ frontend/src/components/Droplet.vue
@@ -1,28 +1,31 @@
 <script setup lang="ts">
 import { defineProps, onMounted, ref } from "vue";
-const { post } = defineProps(["post"]);
+const emit = defineEmits(["update"]);
+const { post, posts } = defineProps(["post", "posts"]);
 const id = `droplet-${post.id}`;
 
 // TODO create emits for velocity to be passed to cloud's array of droplets
 // We then check the array for collisions with other droplets and adjust velocity accordingly
 
-// TODO mass of droplet to be used in collision calculations
-// calculate mass based on length of post content
-
 // random num 1-10 to determine delay for droplet display
 const delay = Math.floor(Math.random() * 10000) + 1000;
 
+const MIN_SPEED = 0.5;
 const SPEED = 2;
 const parentContainer = ref(null);
 let containerWidth = 0;
 let containerHeight = 0;
 let dropletWidth = 0;
 let dropletHeight = 0;
+let mass = post.content.length;
 
 let currentX = 0;
 let currentY = 0;
 let vx = (Math.random() - 0.5) * SPEED;
 let vy = (Math.random() - 0.5) * SPEED;
+
+if (Math.abs(vx) < MIN_SPEED) vx = MIN_SPEED;
+if (Math.abs(vy) < MIN_SPEED) vy = MIN_SPEED;
 
 onMounted(() => {
   const container = document.querySelector(".cloud-container");
@@ -37,6 +40,7 @@ onMounted(() => {
     droplet.classList.remove("hidden");
     droplet.classList.add("fade-in");
 
+    // Get container and droplet dimensions
     containerWidth = container.clientWidth;
     containerHeight = container.clientHeight;
     dropletWidth = droplet.clientWidth;
@@ -44,7 +48,24 @@ onMounted(() => {
     // Random starting position within container bounds
     currentX = Math.floor(Math.random() * (containerWidth - dropletWidth));
     currentY = Math.floor(Math.random() * (containerHeight - dropletHeight));
+
+    emitDroplet();
   }, delay);
+
+  function emitDroplet() {
+    // Populate post with new data
+    emit("update", {
+      content: post.content,
+      id: post.id,
+      x: currentX,
+      y: currentY,
+      vx,
+      vy,
+      mass,
+      dropletWidth,
+      dropletHeight,
+    });
+  }
 
   // Movement loop
   setInterval(() => {
@@ -65,6 +86,8 @@ onMounted(() => {
     // Apply new position
     droplet.style.left = `${currentX}px`;
     droplet.style.top = `${currentY}px`;
+
+    emitDroplet();
   }, 1000 / 60);
 });
 </script>
